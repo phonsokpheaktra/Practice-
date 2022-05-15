af4667da267409a6c341fead0a028b37c8aa95d9
vatanak10 <koeurnsereyvatanak18@kit.edu.kh>
2022-05-15 13:26:12 +0700

    :sparkles: navigation features

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Header from './components/header';
+import Navigator from './routes/homeStack';
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    // <Navigator/>
+    <View>
+      <Header/>
+      <View style={styles.container}>        
+        <Text style={styles.text}>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
     </View>
   );
 }
@@ -17,4 +24,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    paddingTop: 40,
+    color: 'blue',
+  }
 });
