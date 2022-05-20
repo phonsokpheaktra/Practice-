a8ad125805d1dc8dc99d66c1035f29cc71b6e4ed
vatanak10 <koeurnsereyvatanak18@kit.edu.kh>
2022-05-20 13:27:07 +0700

    :heavy_plus_sign: add dropdown picker

--- screens/EditProfile.js
+++ screens/EditProfile.js
@@ -1,12 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, View, Text, TextInput, Image } from 'react-native';
+import DropDownPicker from 'react-native-dropdown-picker';
 
 export default function EditProfile() {
+    const [ name, setName ] = useState('Masha Masha');
+    const [ email, setEmail ] = useState('email@address.com');
+    const [ phone, setPhone ] = useState('088123456');
+    const [ gender, setGender ] = useState(null);    
+    const [ dateOfBirth, setDateOfBirth ] = useState('Masha Masha');
+
+    const [ open, setOpen ] = useState(false);
+    const [items, setItems] = useState([
+        {label: 'Male', value: 'male'},
+        {label: 'Female', value: 'female'}
+      ]);
+
     return (
         <View style={styles.container}>
             <Image style={styles.profile} source={require('../assets/images/midoriya.jpg')}></Image>
-            <View style={styles.subContainer}>
-
+            <View style={styles.subContainer}>                
+                <View style={styles.row}>
+                    <Text style={styles.editText}>Name</Text>
+                    <TextInput 
+                        style={styles.editInput} 
+                        placeholder='Your Name...'
+                        value={name} 
+                        onChangeText={text => setName(text)}
+                    >                        
+                    </TextInput>                    
+                </View>
+                <View style={styles.row}>
+                    <Text style={styles.editText}>Email</Text>
+                    <TextInput 
+                        style={styles.editInput} 
+                        placeholder='Your Email Address...'
+                        value={email} 
+                        onChangeText={text => setEmail(text)}
+                        textContentType='emailAddress'
+                        keyboardType='email-address'
+                    >                        
+                    </TextInput>                    
+                </View>
+                <View style={styles.row}>
+                    <Text style={styles.editText}>Phone</Text>
+                    <TextInput 
+                        style={styles.editInput} 
+                        placeholder='Your Phone Number...'
+                        value={phone} 
+                        onChangeText={text => setPhone(text)}
+                        textContentType='telephoneNumber'
+                        keyboardType='phone-pad'
+                    >                        
+                    </TextInput>                    
+                </View>
+                <View style={styles.row}>
+                    <Text style={styles.editText}>Gender</Text>
+                    <DropDownPicker
+                        open={open}
+                        value={gender}
+                        items={items}
+                        setOpen={setOpen}
+                        setValue={setGender}
+                        setItems={setItems}
+                        // style={{borderWidth: 0,}}
+                        containerStyle={{
+                            width: '50%',
+                            marginBottom: -6,
+                        }}
+                    />
+                </View>
             </View>
         </View>
     )
@@ -32,12 +94,29 @@ const styles = StyleSheet.create({
         width: '95%',
         height: '70%',
         backgroundColor: 'white',
-        padding: 10,        
+        padding: 15,
+        paddingTop: 100,
         borderRadius: 10,
         shadowColor: '#999',
         shadowOffset: { width: 1, height: 2 },
         shadowRadius: 3,
         shadowOpacity: 0.5,
         zIndex: -1,
+    },
+    row: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        borderBottomWidth: 1,
+        padding: 5,
+        margin: 10,
+    },
+    editText: {
+        fontSize: 20,
+        color: 'gray',
+    },
+    editInput:{
+        fontSize: 20,
+        textAlign: 'right',
     },
 });
