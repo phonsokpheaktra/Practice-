a930764e8db043fa15380eb8b04f3c0f8aa2b665
vatanak10 <koeurnsereyvatanak18@kit.edu.kh>
2022-05-22 21:16:48 +0700

    :bookmark: product component

--- screens/home.js
+++ screens/home.js
@@ -17,6 +17,29 @@ export default function Home() {
         iconLink: 'https://img.icons8.com/stickers/100/000000/formal-outfit.png'
       },
   ];
+  const products = [
+    {
+      title: 'Nike Air Max',
+      category: 'Footwear',
+      tag: ['Nike', 'Air Max', 'Fashion', 'Shoes'],
+      price: '$120',
+      imageLink: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+    },
+    {
+      title: 'Nike Air 100',
+      category: 'Shoes',
+      tag: ['Nike', 'Air Max', 'Fashion'],
+      price: '$150',
+      imageLink: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+    },
+    {
+      title: 'Nike Air Max',
+      category: 'Footwear',
+      tag: ['Nike', 'Air Max', 'Fashion'],
+      price: '$120',
+      imageLink: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+    },
+  ];
 
   return (
     <View style={styles.container}>
@@ -42,7 +65,24 @@ export default function Home() {
         </View>
       </View>
       <View style={styles.productContainer}>
-        
+        <View style={styles.productRow}>
+          {products.map((item, index) => {
+            return (
+              <View style={styles.eachProduct}>
+                <Image style={styles.productImage} source={{uri: item.imageLink}}></Image>
+                <Text style={styles.productTitle}>{item.title}</Text>
+                <View style={styles.tagContainer}>
+                  {item.tag.map((tag, index) => {
+                    return (                      
+                      <Text style={styles.productTag}>{tag}</Text>                                        
+                    )
+                  })}
+                </View>
+                <Text style={styles.price}>{item.price}</Text>
+              </View>
+            )
+          })}         
+        </View>
       </View>
     </View>
   );
@@ -88,5 +128,51 @@ const styles = StyleSheet.create({
     },
     eachCategoryTitle: {
       textAlign: 'center',
+    },
+    productContainer: {
+      height: '80%',
+      width: '100%',
+      overflow: 'scroll',
+    },
+    productRow: {
+      justifyContent: 'center',
+      flexDirection: 'row',
+      flexWrap: 'wrap',
+    },
+    eachProduct: {
+      padding: 5,
+      margin: 5,
+      width: 180,
+      // height: 200,
+      borderRadius: 10,
+      backgroundColor: '#fff',
+    },
+    productImage: {
+      height: 120,
+      width: '100%',
+      borderRadius: 10,
+    },
+    productTitle: {
+      marginTop: 3,
+      fontSize: 16,
+    },
+    tagContainer: {
+      marginTop: 3,
+      flexDirection: 'row',
+      flexWrap: 'nowrap',
+      overflow: 'scroll',    
+    },
+    productTag: {
+      whiteSpace: 'nowrap',
+      paddingLeft: 5,
+      paddingRight: 5,
+      color: '#555',
+      backgroundColor: '#ddd',
+      marginEnd: 5,
+      borderRadius: 4,      
+    },
+    price: {
+      fontSize: 22,
+      fontWeight: 'bold',
     },
 });
