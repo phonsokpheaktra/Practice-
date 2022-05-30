a54aec1eca5b25b9740a2fb19b851b41c5194164
yisisovattra <yisisovattra18@kit.edu.kh>
2022-05-30 20:52:13 +0700

    Product Details's Screen

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import AuthStack from './routes/AuthStack';
 import BottomTabs from './routes/TabsNavigator';
 import SearchFilter from './screens/SearchFilter';
+import ProductDetail from './screens/ProductDetail';
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
       <AuthStack />      
     </NavigationContainer>      
     // <SearchFilter/>
+    // <ProductDetail/>
   );
 }
 
