4d8ea7098f0eacfe1c65b429ee4abeb11f2eca38
vatanak10 <koeurnsereyvatanak18@kit.edu.kh>
2022-06-16 21:12:45 +0700

    :wrench: form validation function config

--- components/headerButton.js
+++ components/headerButton.js
@@ -1,77 +1,90 @@
-import { useState } from 'react';
-import { StyleSheet, View, Text, TouchableOpacity, Modal, TextInput, Alert } from 'react-native';
-import axios from '../axios';
-import DropDownPicker from 'react-native-dropdown-picker';
-import Spacing from '../components/Spacing';
+import { useState } from "react";
+import {
+    StyleSheet,
+    View,
+    Text,
+    TouchableOpacity,
+    Modal,
+    TextInput,
+    Alert,
+} from "react-native";
+import axios from "../axios";
+import DropDownPicker from "react-native-dropdown-picker";
+import Spacing from "../components/Spacing";
 
 export default function HeaderButton() {
     const [modalVisible, setModalVisible] = useState(false);
 
-    const [name, setName] = useState('');
-    const [quantity, setQuantity] = useState('');
-    const [price, setPrice] = useState('');
-    const [category, setCategory] = useState('');
-    const [description, setDescription] = useState('');
+    const [name, setName] = useState("");
+    const [quantity, setQuantity] = useState("");
+    const [price, setPrice] = useState("");
+    const [category, setCategory] = useState("");
+    const [description, setDescription] = useState("");
 
-    const [ open, setOpen ] = useState(false);
+    const [open, setOpen] = useState(false);
     const [items, setItems] = useState([
-        { label: 'Electronics', value: 1 },
-        { label: 'Footwear', value: 2 },
-        { label: 'Beauty', value: 3 },
-        { label: 'Apparel', value: 4 },
-        { label: 'Tableware', value: 5 },
-        { label: 'Tools', value: 6 },
+        { label: "Electronics", value: 1 },
+        { label: "Footwear", value: 2 },
+        { label: "Beauty", value: 3 },
+        { label: "Apparel", value: 4 },
+        { label: "Tableware", value: 5 },
+        { label: "Tools", value: 6 },
     ]);
 
     const validation = () => {
         // Empty fields validation
-        if (name === '') {
-            Alert.alert('Product Name Missing!', 'Please Your Product Name...');
+        if (name === "") {
+            Alert.alert(
+                "Product Name Missing!",
+                "Please enter Your Product Name..."
+            );
             return false;
-        } else if (category === '') {
-            Alert.alert('Category Missing!', 'Please Your Category...');
+        } else if (category === "") {
+            Alert.alert("Category Missing!", "Please enter Your Category...");
             return false;
-        } else if (quantity === '') {	
-            Alert.alert('Quantity Missing!', 'Please enter quantity');
+        } else if (quantity === "") {
+            Alert.alert("Quantity Missing!", "Please enter quantity");
             return false;
-        } else if (price === '') {
-            Alert.alert('Price Missing!', 'Please enter price');
+        } else if (price === "") {
+            Alert.alert("Price Missing!", "Please enter price");
             return false;
         }
 
         // Price validation
         if (isNaN(quantity)) {
-            Alert.alert('Quantity is not a number!', 'Please enter a number');
+            Alert.alert("Quantity is not a number!", "Please enter a number");
             return false;
         } else if (isNaN(price)) {
-            Alert.alert('Price is not a number!', 'Please enter a number');
+            Alert.alert("Price is not a number!", "Please enter a number");
             return false;
         }
-    }
+        return true;
+    };
 
     const resetValues = () => {
-        setName('');
-        setCategory('');
-        setQuantity('');
-        setPrice('');
-        setDescription('');
-    }
+        setName("");
+        setCategory("");
+        setQuantity("");
+        setPrice("");
+        setDescription("");
+    };
 
     const postProduct = async () => {
-        validation();
-        axios.post('/api/product/create_product', {
-            product_name: name,
-            quantity: Number(quantity),
-            price: Number(price),
-            categoryId: category,
-            description: description
-        })
-        .then(res => {
-            Alert.alert('Success!', res.data.message);
-            console.log(res.data);
-            setModalVisible(!modalVisible);
-            resetValues();
-        })
+        if (!validation()) return;
+        axios
+            .post("/api/product/create_product", {
+                product_name: name,
+                quantity: Number(quantity),
+                price: Number(price),
+                categoryId: category,
+                description: description,
+            })
+            .then((res) => {
+                Alert.alert("Success!", res.data.message);
+                console.log(res.data);
+                setModalVisible(!modalVisible);
+                resetValues();
+            });
     };
 
     return (
@@ -80,11 +93,9 @@ export default function HeaderButton() {
                 style={styles.button}
                 onPress={() => setModalVisible(true)}
             >
-                <Text style={styles.buttonText}>
-                    Add Product
-                </Text>            
+                <Text style={styles.buttonText}>Add Product</Text>
             </TouchableOpacity>
-            
+
             <Modal
                 animationType="fade"
                 transparent={true}
@@ -95,104 +106,166 @@ export default function HeaderButton() {
             >
                 <View style={styles.centeredView}>
                     <View style={styles.box}>
-                        <Spacing height={20}/>
-                        <Text style={styles.title}>
-                            Add New Product
-                        </Text>
-                        <Spacing height={10}/>
+                        <Spacing height={20} />
+                        <Text style={styles.title}>Add New Product</Text>
+                        <Spacing height={10} />
                         <View style={styles.inputContainer}>
-                            <TextInput style={styles.input} placeholder="Product Name..." value={name} onChangeText={text => setName(text)} selectTextOnFocus={true}/>
+                            <TextInput
+                                style={styles.input}
+                                placeholder="Product Name..."
+                                value={name}
+                                onChangeText={(text) => setName(text)}
+                                selectTextOnFocus={true}
+                            />
                         </View>
-                        <View style={[styles.inputContainer, {zIndex: 1, padding: 0, borderWidth: 0}]}>                    
+                        <View
+                            style={[
+                                styles.inputContainer,
+                                { zIndex: 1, padding: 0, borderWidth: 0 },
+                            ]}
+                        >
                             <DropDownPicker
-                                placeholder='Select Category'
+                                placeholder="Select Category"
                                 open={open}
                                 value={category}
                                 items={items}
                                 setOpen={setOpen}
                                 setValue={setCategory}
-                                setItems={setItems}                        
+                                setItems={setItems}
                                 containerStyle={{
                                     width: "100%",
                                 }}
-                            />  
+                            />
                         </View>
                         <View style={styles.inputContainer}>
-                            <TextInput style={styles.input} placeholder="Quantity..." value={quantity} onChangeText={text => setQuantity(text)} selectTextOnFocus={true}/>
+                            <TextInput
+                                style={styles.input}
+                                placeholder="Quantity..."
+                                value={quantity}
+                                onChangeText={(text) => setQuantity(text)}
+                                selectTextOnFocus={true}
+                            />
                         </View>
                         <View style={styles.inputContainer}>
-                            <TextInput style={styles.input} placeholder="Price..." value={price} onChangeText={text => setPrice(text)} selectTextOnFocus={true}/>
-                        </View>                
-                        <View style={styles.inputContainer}>
-                            <TextInput style={styles.input} placeholder="Description..." value={description} onChangeText={text => setDescription(text)} multiline={true} selectTextOnFocus={true}/>
+                            <TextInput
+                                style={styles.input}
+                                placeholder="Price..."
+                                value={price}
+                                onChangeText={(text) => setPrice(text)}
+                                selectTextOnFocus={true}
+                            />
                         </View>
-                        <View style={{flexDirection: 'row', marginTop: 10}}>
-                            <TouchableOpacity style={[styles.button, {backgroundColor: 'white', borderWidth: 1, borderColor: 'red'}]} onPress={() => {setModalVisible(!modalVisible);}}>
-                                <Text style={[styles.buttonText, {color: 'red'}]}>Cancel</Text>
+                        <View style={styles.inputContainer}>
+                            <TextInput
+                                style={styles.input}
+                                placeholder="Description..."
+                                value={description}
+                                onChangeText={(text) => setDescription(text)}
+                                multiline={true}
+                                selectTextOnFocus={true}
+                            />
+                        </View>
+                        <View style={{ flexDirection: "row", marginTop: 10 }}>
+                            <TouchableOpacity
+                                style={[
+                                    styles.button,
+                                    {
+                                        backgroundColor: "white",
+                                        borderWidth: 1,
+                                        borderColor: "red",
+                                    },
+                                ]}
+                                onPress={() => {
+                                    setModalVisible(!modalVisible);
+                                }}
+                            >
+                                <Text
+                                    style={[
+                                        styles.buttonText,
+                                        { color: "red" },
+                                    ]}
+                                >
+                                    Cancel
+                                </Text>
                             </TouchableOpacity>
-                            <TouchableOpacity style={[styles.button, {backgroundColor: 'green',}]} onPress={() => {postProduct(); }}>
-                                <Text style={[styles.buttonText, {color: 'white'}]}>Confirm</Text>
+                            <TouchableOpacity
+                                style={[
+                                    styles.button,
+                                    { backgroundColor: "green" },
+                                ]}
+                                onPress={() => {
+                                    postProduct();
+                                }}
+                            >
+                                <Text
+                                    style={[
+                                        styles.buttonText,
+                                        { color: "white" },
+                                    ]}
+                                >
+                                    Confirm
+                                </Text>
                             </TouchableOpacity>
                         </View>
-                        <Spacing height={20}/>
+                        <Spacing height={20} />
                     </View>
                 </View>
             </Modal>
         </View>
-    )
+    );
 }
 
 const styles = StyleSheet.create({
     mainButton: {
-        backgroundColor: 'transparent',        
+        backgroundColor: "transparent",
         // paddingRight: 10,
     },
     buttonText: {
-        color: '#1c1c1e',
+        color: "#1c1c1e",
         fontSize: 18,
-        fontWeight: '500',
-        textAlign: 'center',    
+        fontWeight: "500",
+        textAlign: "center",
     },
     centeredView: {
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
         padding: 20,
-        backgroundColor: 'rgba(0,0,0,0.5)',
+        backgroundColor: "rgba(0,0,0,0.5)",
     },
     box: {
-        backgroundColor: 'white',
+        backgroundColor: "white",
         // height: 480,
         width: 300,
         borderRadius: 15,
-        justifyContent: 'center',
-        alignItems: 'center',
-        shadowColor: '#666',
+        justifyContent: "center",
+        alignItems: "center",
+        shadowColor: "#666",
         shadowOffset: { width: 2, height: 2 },
         shadowRadius: 3,
     },
     title: {
         fontSize: 25,
-        fontWeight: 'bold',
-        textAlign: 'center',
+        fontWeight: "bold",
+        textAlign: "center",
     },
-    inputContainer:{
-        width: '80%',
+    inputContainer: {
+        width: "80%",
         borderRadius: 8,
-        flexDirection: 'row',
-        alignItems: 'center',
+        flexDirection: "row",
+        alignItems: "center",
         borderWidth: 1,
-        borderColor: '#666',
+        borderColor: "#666",
         padding: 9,
         margin: 10,
     },
     input: {
-        width: '90%'
+        width: "90%",
     },
     button: {
-        justifyContent: 'center',
+        justifyContent: "center",
         width: 120,
-        height: 40,        
+        height: 40,
         margin: 5,
         borderRadius: 10,
     },
